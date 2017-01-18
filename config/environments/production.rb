b9824870557b925139cf5c480c61541c1e8278f3
Jacob Lester <lester1423@gmail.com>
2017-01-18 15:42:41 +0000

    email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,22 +68,20 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-
-  # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'https://peaceful-dusk-49968.herokuapp.com/' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "https://peaceful-dusk-49968.herokuapp.com/",
+    domain: Rails.application.secrets.domain_name,
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: Rails.application.secrets.email_provider_username,
+    password: Rails.application.secrets.email_provider_password
   }
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
 
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
